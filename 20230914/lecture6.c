089657e07a6060f65527674d8496cf3dd195b38c
olekirk123 <olekirk@naver.com>
2023-09-14 11:20:04 +0900

    num

---
+++ 20230914/lecture6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int input_num;
+	printf("정수 입력:");
+	scanf_s("d", &input_num);
+
+	if (input_num > 0)
+		printf("양의 정수 (%d)\n", input_num);
+	else if (input_num < 0)
+		printf("음의 정수 (%d)\n", input_num);
+	else
+		printf("0");
+	return 0;
+}
