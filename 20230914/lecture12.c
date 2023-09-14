f615c28515f61aaf2d2aaa35bc9619fe7dd4b59c
olekirk123 <olekirk@naver.com>
2023-09-14 12:26:54 +0900

    oxoxo

---
+++ 20230914/lecture12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 5; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			printf("    ");
+		}
+		if (a % 2 == 0) printf("| O |");
+		else printf("| X |");
+		printf("\n--------------------------\n");
+	}
+	return 0;
+}
