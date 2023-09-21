c584d70bd3bc48210767fdb08b91935899061a28
olekirk123 <olekirk@naver.com>
2023-09-21 10:08:54 +0900

    Update lecture2.c

--- 20230921/lecture2.c
+++ 20230921/lecture2.c
@@ -3,8 +3,6 @@
 int main(void) {
 	int sel, num1, num2;
 
-	double num1, num2;
-
 	printf("숫자 1을 입력하시오 : ");
 	scanf_s("%lf", &num1);
 
@@ -14,10 +12,11 @@ int main(void) {
 	printf("1.더하기 2.빼기 3.곱하기 4.나누기 : ");
 	scanf_s("%d", &sel);
 
-	calculator(sel);
+	calculator(sel,num1,num2);
 
 	return 0;
 }
+void calculatro(int selctor)
 {
 	if (selector == 1) sum(num1, num2);
 	else if (selector == 2) sub(num1, num2);
